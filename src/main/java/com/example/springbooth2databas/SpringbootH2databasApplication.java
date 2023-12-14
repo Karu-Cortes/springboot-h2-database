527ce21061e6c4fa6791e376fbc6dbc69d5e5a19
KarenCortes <karenj.cortesn@gmail.com>
2023-12-14 10:25:39 -0500

    implementación h2 database

---
+++ src/main/java/com/example/springbooth2databas/SpringbootH2databasApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbooth2databas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootH2databasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootH2databasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbooth2databas/SpringbootH2databasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbooth2databas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootH2databasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
